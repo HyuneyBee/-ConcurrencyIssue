5d4110fbeec98a94c45c5f6bda8a9dfd586517d8
HyuneyBee <gsh2448@gmail.com>
2023-06-08 21:49:49 +0900

    @Service, @Repository 어노테이션 추가

--- src/main/java/com/example/lock/repository/StockRepository.java
+++ src/main/java/com/example/lock/repository/StockRepository.java
@@ -2,6 +2,8 @@ package com.example.lock.repository;
 
 import com.example.lock.model.Stock;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface StockRepository extends JpaRepository<Stock, Long> {
 }
--- src/main/java/com/example/lock/service/StockService.java
+++ src/main/java/com/example/lock/service/StockService.java
@@ -4,7 +4,9 @@ import com.example.lock.model.Stock;
 import com.example.lock.repository.StockRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
+@Service
 @RequiredArgsConstructor
 public class StockService {
     private final StockRepository stockRepository;
